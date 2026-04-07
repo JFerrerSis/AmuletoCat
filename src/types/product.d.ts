d594adf695428980148f1b3885757b3c96cd0cb5
jferrer <jorgeferrercohen@gmail.com>
2026-04-07 15:03:31 -0400

    ordenado de subcategorias

--- src/types/product.d.ts
+++ src/types/product.d.ts
@@ -1,9 +1,19 @@
+// src/types/product.ts
+
+export type SubcategoriaPulsera = 
+  | "AMOR" 
+  | "ESENCIALES" 
+  | "Fe" 
+  | "PROTECCION" 
+  | "VENEZUELA";
+
 export interface Product {
   id: string;
   nombre: string;
   precio: number;
   imagen: string;
-  categoria: string;
+  categoria: string; // Flexible para añadir más categorías después
+  subcategoria: SubcategoriaPulsera | string; // Permite las actuales y nuevas que añadas
   descripcion: string;
 }
 

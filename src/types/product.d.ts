d204d644876c0773cd65009574a9d4e374476f87
jferrer <jorgeferrercohen@gmail.com>
2026-04-09 09:17:40 -0400

    ajuste catalogo carruel por unidad

--- src/types/product.d.ts
+++ src/types/product.d.ts
@@ -5,15 +5,17 @@ export type SubcategoriaPulsera =
   | "ESENCIALES" 
   | "Fe" 
   | "PROTECCION" 
-  | "VENEZUELA";
+  | "VENEZUELA"
+  | "GENERAL"; // Añadí GENERAL para los rosarios
 
 export interface Product {
   id: string;
   nombre: string;
   precio: number;
   imagen: string;
-  categoria: string; // Flexible para añadir más categorías después
-  subcategoria: SubcategoriaPulsera | string; // Permite las actuales y nuevas que añadas
+  imagenes?: string[]; // <--- AÑADE ESTA LÍNEA (la '?' indica que es opcional)
+  categoria: string;
+  subcategoria: SubcategoriaPulsera | string;
   descripcion: string;
 }
 
